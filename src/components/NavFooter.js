60bee8eb79b29fe984fba8fd866cbe1e23d75e28
Adi Vrskic <adivrskic123@gmail.com>
2021-08-10 20:27:20 -0400

    Update footer a little

--- src/components/NavFooter.js
+++ src/components/NavFooter.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavContext } from "../context/navContext";
 import data from "../data/profile.json";
 import "../styles/components/NavFooter.scss";
 
 export const NavFooter = () => {
-  return <div className="nav-footer">{data.userName}</div>;
+  const [state] = React.useContext(NavContext);
+
+  return (
+    <div className="nav-footer">
+      <div className={`nav-footer__icon ${state.navOpen && "open"}`}></div>
+      <span className="nav-footer__text">{data.userName}</span>
+    </div>
+  );
 };
